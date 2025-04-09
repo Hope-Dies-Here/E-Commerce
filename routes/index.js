24132a78129d24f2aff97280a01811588be2d2a2
hope-dies-here <onyourleftpal@gmail.com>
2025-04-09 11:01:01 +0300

    for ejs

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const supabase = require("../config/database");
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    // const response = await fetch('https://fakestoreapi.com/products')
+    // const response = await fetch('https://api.escuelajs.co/api/v1/products')
+    // const data = await response.json()
+    // console.log(data)
+    // const response = await fetch("http://localhost:3000/api/products");
+    // const localData = await response.json();
+    // console.log(localData);
+    const { data, error } = await supabase.from("products").select("*");
+
+    if (error) {
+      return response.error(res, "Error fetching product");
+    }
+
+    res.render("index", { data });
+  } catch (error) {
+    console.log(error);
+    res.render("Welp, try again");
+  }
+});
+
+module.exports = router;
